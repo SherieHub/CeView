c3975080fd4acf190241e2ca6e9332e95eaf809c
Jamjamzz <codejamzz27@gmail.com>
2026-05-27 01:35:47 +0800

    feat: agent classification

--- backend/fastapi-sbert/app/services/caption_generation.py
+++ backend/fastapi-sbert/app/services/caption_generation.py
@@ -1,6 +1,6 @@
-from agents.creative_director_agent.graph import caption_generation_agent
-from model.CaptionInputClass import CaptionInputClass
+from app.agents.creative_director_agent.graph import caption_generation_agent
+from app.model.CaptionInputClass import CaptionInputClass
 
-async def caption_generation(input: CaptionInputClass):
+async def caption_generation_service(input: CaptionInputClass):
     result = await caption_generation_agent.ainvoke(input)
     return result
